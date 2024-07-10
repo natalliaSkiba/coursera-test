b6866054634cf9614c1a49633a9353c6fb56f924
natalliaSkiba <natalliaSkiba@users.noreply.github.com>
2024-07-10 10:03:00 +0000

    deploy: e05c412455dfb3ac3f480e4b758bfa1bd238306e

--- app.js
+++ app.js
@@ -1,29 +1,62 @@
-(function () {
-'use strict';
+(function (){
+  'use strict';
 
-angular.module('LunchCheck', [])
+  angular.module('ShoppingListCheckOff', [])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-.controller('LunchCheckController', LunchCheckController);
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService){
+  var listToBuy = this;
 
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController($scope){
-  $scope.items = "";
-  $scope.message ="";
+//Get the items to buy from the service
+  listToBuy.items = ShoppingListCheckOffService.getToBuyItems();
 
-  $scope.checkItems = function() {
-    if ($scope.items.trim() === ""){
-      $scope.message = "Please entre data first";
-    }else {
-      var itemsArray = $scope.items.split(',').filter(function(items) {
-        return items.trim()!=="";
-      });
-      if (itemsArray.length <= 3) {
-        $scope.message = "Enjoy!";
-
-      } else {
-        $scope.message = "Too much!";
-      }
-    }
+//Define the method to move items to the bought list
+  listToBuy.buyItem = function(itemIndex){
+    ShoppingListCheckOffService.buyItem(itemIndex);
   };
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService){
+var listBought = this;
+
+//Get the bought items to the service
+listBought.items = ShoppingListCheckOffService.getBoughtItems();
+}
+
+//Define the service to manage the Shopping lists
+function ShoppingListCheckOffService(){
+  var service = this;
+
+//Initial list of items to buy
+  var toBuyItems = [
+    { name: "cookies", quantity: 10 },
+      { name: "Cola", quantity: 15 },
+      { name: "chips", quantity: 2 },
+      { name: "milk", quantity: 20 },
+      { name: "chocolate", quantity: 5 }
+  ];
+
+//Empty list for bought items
+  var boughtItems = [];
+
+//Method to get the items to buy
+  service.getToBuyItems = function () {
+    return toBuyItems;
+  };
+
+  //Metod to get the bought items
+  service.getBoughtItems = function(){
+    return boughtItems;
+  }
+
+//Method to move an item from to-buy list to the bought list
+  service.buyItem = function(itemIndex) {
+    var item = toBuyItems.splice(itemIndex, 1)[0];
+  boughtItems.push(item);
+};
 }
 })();
